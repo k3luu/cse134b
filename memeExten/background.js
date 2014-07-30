e369f705b9b61d32a23cb9c83a9b4213ec57aa83
avarnau <avarnau@ucsd.edu>
2014-07-29 19:50:54 -0700

    modified extension for correct click operation

--- memeExten/background.js
+++ memeExten/background.js
@@ -1,16 +1,20 @@
-// Set up context menu at install time.
-chrome.contextMenus.create( {
-    'title': 'Add to MemeMaster Library',
-    'contexts': ['image'],
-    'onclick': onClickHandler
+chrome.runtime.onInstalled.addListener(function() {
+ var context = "image";
+ var title = "Add to MemeMaster Library";
+ var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+ "id": "context" + context});
 });
+
+// add click event
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
     var contents = {}
     contents.url = info.srcUrl;
     
-    var url = "addWindow.html";  
+    var url = "http://cse134-135-2014.github.io/cse134_group19/addPage.html";  
     chrome.windows.create({ url: url}, function() {
         chrome.runtime.sendMessage({ contents: contents}, function(response) {});
 });
