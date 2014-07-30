1da5552d6665088ad453d02c3a2b8c8aeb97381f
avarnau <avarnau@ucsd.edu>
2014-07-29 19:40:02 -0700

    added new explore page and extension work
    explore2.html is formatted using a list instead of a table; chrome
    extension work is added

--- memeExten/background.js
+++ memeExten/background.js
@@ -1,17 +1,17 @@
 // Set up context menu at install time.
-chrome.runtime.onInstalled.addListener(function() {
-  var context = "image";
-  var title = "Add to MemeMaster Library";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+chrome.contextMenus.create( {
+    'title': 'Add to MemeMaster Library',
+    'contexts': ['image'],
+    'onclick': onClickHandler
 });
-
-// add click event
-chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-  //var sText = info.selectionText;
-  var url = "http://cse134-135-2014.github.io/cse134_group19/addPage.html";  
-  window.open(url, '_blank');
-};
+    var contents = {}
+    contents.url = info.srcUrl;
+    
+    var url = "addWindow.html";  
+    chrome.windows.create({ url: url}, function() {
+        chrome.runtime.sendMessage({ contents: contents}, function(response) {});
+});
+}
