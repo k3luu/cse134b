8fecbb816c009e8aa77966403ec588670b34ceb2
avarnau <avarnau@ucsd.edu>
2014-07-30 20:53:02 -0700

    adding parse XMLHttpRequest functionality

--- memeExten/background.js
+++ memeExten/background.js
@@ -11,9 +11,29 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
+    var xml = new XMLHttpRequest();
+    xml.onload = function() {
+        var yml = new XMLHttpRequest();
+        yml.responseType = 'json';
+        yml.onload = function() {
+            console.log(yml.response.url);
+        };
+        yml.open('POST', 'https://api.parse.com/1/files/test.jpg');
+        yml.setRequestHeader("X-Parse-Application-Id", "8lnVjjPew8kds7OwHV3j0VDtO7eqiQXNtv1mwACQ");
+        yml.setRequestHeader("X-Parse-REST-API-Key", "AUVrB3v6VV2utjGmJifyWeBaZHIDbfdvR7NM30VA");
+        yml.setRequestHeader("Content-Type", "image/jpeg");
+        console.log(yml);
+        console.log(xml.response);
+        yml.send(xml.response);
+    
+        //yml.sendAsBinary(xml.response);
+    };
+    xml.responseType = 'blob';
+    xml.open('GET', info.srcUrl);
+    xml.send();
+    
     var contents = {}
     contents.url = info.srcUrl;
-    
     var url = "addPage.html";  
     chrome.tabs.create({ url: url}, function() {
         chrome.runtime.sendMessage({ contents: contents}, function(response) {});
